723b19f41db195e0d273d249799516228f9aa3f8
Neerajjv <154986859+Neerajjv@users.noreply.github.com>
2024-06-07 17:13:43 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ import streamlit as st
 from HELPER import get_qa_chain, create_vector_db
 
 st.title("Your Data Science Helper 📖💡🔍 ")
-st.title("zieganpalg")
-image = "1.jpg"
-st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.title("Without questions there can be no learning")
+#image = "1.jpg"
+#st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 question = st.text_input("Question: ")
 
 if question:
